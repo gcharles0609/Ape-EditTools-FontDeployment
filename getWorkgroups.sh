1192eda55c805a9359951a772fa4fd6e86ba0be3
burhanhamid <burhan.hamid@timeinc.com>
2015-08-24 09:51:30 -0400

    shell script and updated test file

---
+++ getWorkgroups.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+sudo -u _www corecli -clientserver -login server=uts3.timeinc.com port=8080 username=fontdeploy password=Time2015
+sudo -u _www corecli -clientserver -getWorkgroups
--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
+$utcServer = "uts3.timeinc.com";
+$utcPort = "8080";
+$utcUser = "fontdeploy";
+$utcPassword = "Time2015";
+
+shell_exec('sudo corecli -clientserver -login server='.$utcServer.' port='.$utcPort.' username='.$utcUser.' password='.$utcPassword);
+$workGroups = shell_exec('sudo corecli -clientserver -getWorkgroups');
+print_r ($workGroups);
 ?>
