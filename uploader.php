0d4df3f88fa89a3d2af123a0a18ca175d3c7eaa0
burhanhamid <burhan.hamid@timeinc.com>
2015-08-27 12:31:59 -0400

    Added drop zone.js for easier font upload

--- uploader.php
+++ uploader.php
@@ -2,8 +2,8 @@
 $brandName = $_POST["fontset"];
 // Need to create the uploads folder on your workstation and change permissions on it to 777
 
-$target_dir = "/Users/Shared/uploads/$brandName/$brandName";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_dir = "/Users/Shared/uploads/$brandName/$brandName/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
@@ -35,8 +35,8 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
